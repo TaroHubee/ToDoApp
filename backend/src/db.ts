dcea9a34f1f73ceb4cbd547c4757bef40641ee88
Taro-HITOMI <hitomitaro2000@gmail.com>
2025-11-25 20:58:39 +0900

    add: files [#15]

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,11 @@
 import sqlite3 from 'sqlite3';
 import {open} from 'sqlite';
+import path from "path";
+import { fileURLToPath } from "url";
 
 export const connectDB = async () => {
   const db = await open({
-    filename: './database.db',
+    filename: path.join(__dirname, "../../database/database.db"),
     driver: sqlite3.Database
   });
   // tasks テーブルがなければ作成
@@ -13,7 +15,7 @@ export const connectDB = async () => {
       task TEXT,
       categoryId INTEGER,
       due TEXT,
-      status TEXT
+      statusId INTEGER
     )
   `);
   await db.exec(`
@@ -25,15 +27,20 @@ export const connectDB = async () => {
   await db.exec(`
     CREATE TABLE IF NOT EXISTS status (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      status TEXT
+      status TEXT NOT NULL UNIQUE
     )
   `);
-  // await db.exec(`
-  //   DROP TABLE IF EXISTS tasks
-  // `); // usersテーブルを削除
-  // await db.exec(`
-  //   DROP TABLE IF EXISTS category
-  // `); // usersテーブルを削除
+  await db.exec(`
+    DROP TABLE IF EXISTS tasks
+  `); // usersテーブルを削除
+  await db.exec(`
+    DROP TABLE IF EXISTS category
+  `); // usersテーブルを削除
+  await db.exec(`
+    DROP TABLE IF EXISTS status
+  `); // usersテーブルを削除
+
+  
   return db;
 }
 
